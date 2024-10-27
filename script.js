10d09307969eba99e768499b20d0f1a9757a61b7
Sean Tran <141064686+Guitar-Fan@users.noreply.github.com>
2024-10-27 23:16:58 +0000

    css update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-<script>
-
-</script>
+document.getElementById('schedule-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('time').value;
+    alert(`Schedule submitted for ${date} at ${time}`);
+  });
